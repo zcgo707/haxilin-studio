897f9da44d21a8f322f4ecc5ccf58a185826abc1
HaxiLin <zen707@qq.com>
2026-07-09 19:01:31 +0800

    v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://x.zcgo.top",
-  adapter: cloudflare(),
   output: "static",
-  integrations: [preact()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["astro:content", "astro/loaders", "astro/zod"],
+    },
+  },
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
-
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 export async function GET(context) {
+  const posts = await getCollection("blog");
   return rss({
     title: "HaxiLin Studio",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.id}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
